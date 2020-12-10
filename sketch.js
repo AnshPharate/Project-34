e0d25ae159124be3b111e6dfdb924f2984db7663
AnshPharate <72381637+AnshPharate@users.noreply.github.com>
2020-12-10 09:34:10 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,70 +1,57 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
-const body = Matter.Body;
-const Render= Matter.Render;
+const Mouse = Matter.Mouse;
 const Constraint = Matter.Constraint;
 
-var suspender1,suspender2,suspender3,suspender4,suspender5;
+const MouseConstraint = Matter.MouseConstraint;
+
+var world, engine, canvas;
+var pendulum1, pendulum2, pendulum3, pendulum4, pendulum5;
+var sling1, sling2, sling3, sling4, sling5;
+var mConstraint;
+
 function setup() {
-	createCanvas(800, 400);
+  canvas = createCanvas(windowWidth / 2, windowHeight / 1.5);
+  engine = Engine.create();
+  world = engine.world;
 
+  let canvasmouse = Mouse.create(canvas.elt);
+  canvasmouse.pixelRatio = pixelDensity();
+  let options = {
+    mouse: canvasmouse
+  };
+  mConstraint = MouseConstraint.create(engine, options);
+  World.add(world, mConstraint);
 
-	engine = Engine.create();
-	world = engine.world;
+  pendulum1 = new Pendulum(340, 450, "#00b0ff");
+  pendulum2 = new Pendulum(400, 450, "#e91e63");
+  pendulum3 = new Pendulum(460, 450, "#ffc107");
+  pendulum4 = new Pendulum(520, 450, "#e91e63");
+  pendulum5 = new Pendulum(580, 450, "#00b0ff");
 
-	//Create the Bodies Here.
-//suspension=createSprite(290,200,600,200);
-bob1=new Bob(300,350,50);
-bob2=new Bob(350,350,50);
-bob3=new Bob(400,350,50);
-bob4=new Bob(450,350,50);
-bob5=new Bob(500,350,50);
-//bob6=new Bob(150,250,30);
- //fill ("brown");
-  ground1 = new Ground(400,100,300,40);
-  suspender1= new Suspender(bob1.body,ground1.body,-50*2,0);
-  suspender2= new Suspender(bob2.body,ground1.body,-25*2,0);
-  suspender3= new Suspender(bob3.body,ground1.body,-0*2,0);
-  suspender4= new Suspender(bob4.body,ground1.body,25*2,0);
-  suspender5= new Suspender(bob5.body,ground1.body,50*2,0);
-  //sling6= new Suspender(bob6.body,ground1.body,-40*2,0);
-	Engine.run(engine);
-  
+  sling1 = new Sling(pendulum1.body, { x: 340, y: 200 });
+  sling2 = new Sling(pendulum2.body, { x: 400, y: 200 });
+  sling3 = new Sling(pendulum3.body, { x: 460, y: 200 });
+  sling4 = new Sling(pendulum4.body, { x: 520, y: 200 });
+  sling5 = new Sling(pendulum5.body, { x: 580, y: 200 });
 }
 
-
 function draw() {
-  background(100);
-//	ground.fill(48,22,8);
-	//ground.stroke(0);
-  rectMode(CENTER)
+  background(0);
   Engine.update(engine);
-  bob1.display();
-  bob2.display();
-  bob3.display();
-  bob3.display();
-  bob4.display();
-  bob5.display();
-  //bob6.display();
-  ground1.display();
+  pendulum1.display();
+  pendulum2.display();
+  pendulum3.display();
+  pendulum4.display();
+  pendulum5.display();
   sling1.display();
   sling2.display();
   sling3.display();
   sling4.display();
   sling5.display();
-  //sling6.display();
-
-  keyPressed();
-  drawSprites ();
 }
 
-function keyPressed() { 
-	if (keyCode === UP_ARROW) 
-	{ Matter.Body.applyForce(bob1.body,bob1.body.position,{x:-100,y:100}); } 
-} 
-
-
-
-
+function mouseDragged() {
+  Matter.Body.setPosition(pendulum1.body, { x: mouseX, y: mouseY });
+}
